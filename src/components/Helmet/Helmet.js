5682ff3e62ff9b4cb836d0a3101c43c4a04c72a3
thtshajjad <shagors@users.noreply.github.com>
2023-06-26 11:42:52 +0600

    hero section design and fully checked

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "MultiMart - " + props.ttitle;
+  document.title = "MultiMart - " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Col, Container, Row } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
+import "../styles/home.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg={6} md={6}>
+              <div className="hero__content">
+                <p className="hero__subtitle">Treding Product in {year}</p>
+                <h2>Make your Outfit More Minimalistic & Modern</h2>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Ipsum impedit error unde, praesentium cupiditate suscipit
+                  delectus ratione eos laborum amet?
+                </p>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg={6} md={6}>
+              <div className="hero__img">
+                <img src={heroImg} alt="HeroImage" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
