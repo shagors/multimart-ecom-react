dd0c47814756bfd6a5c96eaa0897346493d1f6e1
thtshajjad <shagors@users.noreply.github.com>
2023-06-26 17:09:41 +0600

    footer structure add and set footer menu

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,101 @@
 import React from "react";
+import "./footer.css";
+import { Col, Container, ListGroup, ListGroupItem, Row } from "reactstrap";
+import logo from "../../assets/images/eco-logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="Brand" />
+              <div>
+                <h1>MultiMart</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              aut reiciendis rerum suscipit repellat blanditiis magnam,
+              dignissimos inventore possimus distinctio!
+            </p>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                  </span>
+                  <p>123, Rangpur, Bangladesh</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                  </span>
+                  <p>+8801770877400</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                  </span>
+                  <p>shajjadshagor@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="footer__copywright">
+              Copywright {year} developed by Shajjad Shagor. All rights
+              reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
