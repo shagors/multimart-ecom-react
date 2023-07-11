b674019970382763ddb1da37ddd78729a28f3903
thtshajjad <shagors@users.noreply.github.com>
2023-07-11 15:22:23 +0600

    some file changes and shop page add select options add and design it

--- src/components/UI/CommonSection.jsx
+++ src/components/UI/CommonSection.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Container } from "reactstrap";
+import "../../styles/common-section.css";
 
-const CommonSection = () => {
-  return <div>CommonSection</div>;
+const CommonSection = ({ title }) => {
+  return (
+    <section className="common__section">
+      <Container className="text-center">
+        <h1>{title}</h1>
+      </Container>
+    </section>
+  );
 };
 
 export default CommonSection;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import { Col, Container, Row } from "reactstrap";
+import "../styles/shop.css";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <Helmet title={Shop}>
+      <CommonSection title="Products" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="3">
+              <div className="filter__widget">
+                <select name="" id="">
+                  <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="3" md="3">
+              <div className="filter__widget">
+                <select name="" id="">
+                  <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="search__box">
+                <input type="text" placeholder="Search........" />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Shop;
