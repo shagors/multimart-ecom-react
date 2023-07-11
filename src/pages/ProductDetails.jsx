f21af1ec47b1088714d6e5afe751cda028d628b3
thtshajjad <shagors@users.noreply.github.com>
2023-07-11 16:58:18 +0600

    product details page add products image and descriptions and details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import products from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import { Col, Container, Row } from "reactstrap";
+import { useParams } from "react-router-dom";
+import "../styles/product-details.css";
+import { motion } from "framer-motion";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+
+  const {
+    imgUrl,
+    productName,
+    price,
+    avgRating,
+    review,
+    description,
+    shortDesc,
+  } = product;
+
+  return (
+    <Helmet title={productName}>
+      <CommonSection title={productName} />
+      <section className="pt-0">
+        <Container>
+          <Row>
+            <Col lg="6">
+              <img src={imgUrl} alt="Product-Image" />
+            </Col>
+            <Col lg="6">
+              <div className="product__details">
+                <h2 className="">{productName}</h2>
+                <div className="product__rating d-flex align-items-center gap-5 mb-3">
+                  <div>
+                    <span>
+                      <i className="ri-star-s-fill"></i>
+                    </span>
+                    <span>
+                      <i className="ri-star-s-fill"></i>
+                    </span>
+                    <span>
+                      <i className="ri-star-s-fill"></i>
+                    </span>
+                    <span>
+                      <i className="ri-star-s-fill"></i>
+                    </span>
+                    <span>
+                      <i className="ri-star-half-s-fill"></i>
+                    </span>
+                  </div>
+
+                  <p>
+                    (<span>{avgRating}</span> ratings)
+                  </p>
+                </div>
+
+                <span className="product__price">${price}</span>
+                <p className="mt-3">{shortDesc}</p>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  Add to Cart
+                </motion.button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default ProductDetails;
