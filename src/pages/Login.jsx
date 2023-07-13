4efb5bd8c0c46f44ca036438a6579c583d3e73fb
thtshajjad <shagors@users.noreply.github.com>
2023-07-13 16:53:51 +0600

    design login and sign up page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Col, Container, Form, FormGroup, Row } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+import { motion } from "framer-motion";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Helmet title="Login">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold mb-4">Login</h3>
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    name=""
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="passwprd"
+                    name=""
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+
+                <motion.button
+                  type="submit"
+                  whileTap={{ scale: 0.9 }}
+                  className="buy__btn auth__btn">
+                  Login
+                </motion.button>
+                <p>
+                  Don't have an account?{" "}
+                  <Link to="/signup">Create an account</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Col, Container, Form, FormGroup, Row } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+import { motion } from "framer-motion";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [file, setFile] = useState(null);
+
+  return (
+    <Helmet title="Signup">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold mb-4">Signup</h3>
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Enter your Name"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    name=""
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="passwprd"
+                    name=""
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </FormGroup>
+
+                <motion.button
+                  type="submit"
+                  whileTap={{ scale: 0.9 }}
+                  className="buy__btn auth__btn">
+                  Create an Account
+                </motion.button>
+                <p>
+                  Already have an account?
+                  <Link to="/login"> Login</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Signup;
