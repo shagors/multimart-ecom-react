d8adc6a4f32189d3da989a7bb3d1faf6c7f8b349
thtshajjad <shagors@users.noreply.github.com>
2023-07-17 16:39:21 +0600

    use hook for data collecti from firebase and those data collet and check with console

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import productImg from "../assets/images/arm-chair-01.jpg";
+import useGetData from "../hooks/useGetData";
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const { data: productsData } = useGetData("products");
+
+  console.log(productsData);
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={productImg} alt="Product-img" />
+                  </td>
+                  <td>Arm Chair</td>
+                  <td>Chair</td>
+                  <td>$150</td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default AllProducts;
---
+++ src/hooks/useGetData.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase.config";
+import { collection, getDocs } from "firebase/firestore";
+
+const useGetData = (collectionName) => {
+  const [data, setData] = useState([]);
+  const collectionRef = collection(db, collectionName);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDocs(collectionRef);
+      setData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getData();
+  }, []);
+
+  return { data };
+};
+
+export default useGetData;
