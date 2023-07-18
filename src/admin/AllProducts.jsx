30618897ae1e34bc754605d8b826421b3b0dea92
thtshajjad <shagors@users.noreply.github.com>
2023-07-18 16:29:26 +0600

    add products and delete from realtime server check it work properly

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
-import productImg from "../assets/images/arm-chair-01.jpg";
 import useGetData from "../hooks/useGetData";
+import { db } from "../firebase.config";
+import { doc, deleteDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const AllProducts = () => {
-  const { data: productsData } = useGetData("products");
+  const { data: productsData, loading } = useGetData("products");
 
-  console.log(productsData);
+  const deleteProduct = async (id) => {
+    await deleteDoc(doc(db, "products", id));
+    toast.success("Deleted product!");
+  };
+
+  // console.log(productsData);
   return (
     <section>
       <Container>
@@ -23,17 +30,31 @@ const AllProducts = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>
-                    <img src={productImg} alt="Product-img" />
-                  </td>
-                  <td>Arm Chair</td>
-                  <td>Chair</td>
-                  <td>$150</td>
-                  <td>
-                    <button className="btn btn-danger">Delete</button>
-                  </td>
-                </tr>
+                {loading ? (
+                  <tr>
+                    <td>
+                      <h3 className="text-center py-5 fw-bold">Loading.....</h3>
+                    </td>
+                  </tr>
+                ) : (
+                  productsData?.map((item) => (
+                    <tr key={item.id}>
+                      <td>
+                        <img src={item.imgUrl} alt="Product-img" />
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.category}</td>
+                      <td>{item.price}</td>
+                      <td>
+                        <button
+                          onClick={() => deleteProduct(item.id)}
+                          className="btn btn-danger">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </Col>
